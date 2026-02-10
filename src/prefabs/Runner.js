17aa956c2b1361ed9dd47dde529d2b79195ce20d
GqbeBolt <95443729+GqbeBolt@users.noreply.github.com>
2026-02-10 10:54:23 -0800

    player added

--- src/prefabs/Runner.js
+++ src/prefabs/Runner.js
@@ -4,11 +4,19 @@ class Runner extends Phaser.GameObjects.Sprite {
 
         // add object to existing scene
         scene.add.existing(this);
+        scene.physics.add.existing(this);
+        
+        this.body.setCollideWorldBounds(true);
         
         scene.runnerState = new StateMachine("running", {
             running: new RunningState(),
             jumping: new JumpState()
         }, [scene, this]);
+
+        scene.colorState = new StateMachine("red", {
+            red: new RedState(),
+            blue: new BlueState()
+        }, [scene, this])
     
     }
 
@@ -30,6 +38,26 @@ class RunningState extends State {
 }
 
 class JumpState extends State {
+    enter(scene, runner) {
+
+    }
+
+    execute(scene, runner) {
+        // handle transitions
+    }
+}
+
+class RedState extends State {
+    enter(scene, runner) {
+
+    }
+
+    execute(scene, runner) {
+        // handle transitions
+    }
+}
+
+class BlueState extends State {
     enter(scene, runner) {
 
     }
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,26 +4,14 @@ class Play extends Phaser.Scene {
     }
 
     create() {
-        
+        this.runner = new Runner(this, 100, 300, "runner", 0);
+
+        this.physics.world.gravity.y = 2000;
     }
 
     update() {
 
         
     }
-
-    checkCollision(rocket, ship) {
-        // simple AABB checking
-        if (rocket.x < ship.x + ship.width &&
-            rocket.x + rocket.width > ship.x &&
-            rocket.y < ship.y + ship.height &&
-            rocket.height + rocket.y > ship.y) {
-
-            return true;
-        } else {
-            return false;
-        }
-    }
-
     
 }
