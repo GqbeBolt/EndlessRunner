ab559db31001320403f75cd1488627567c3e9c1d
GqbeBolt <95443729+GqbeBolt@users.noreply.github.com>
2026-02-12 16:59:25 -0800

    idk smth

--- src/prefabs/Platform.js
+++ src/prefabs/Platform.js
@@ -3,8 +3,16 @@ class Platform{
         //super(scene, x, y, "platform", 0);
 
         
-        let currCorner = scene.add.sprite(x, y, "platCorner").setOrigin(0);
-        let currSide = scene.add.tileSprite(x, y+10, 10, height-20, "platSide").setOrigin(0);
+        let firstCorner = scene.add.sprite(x, y, "platCorner").setOrigin(0);
+        let firstSide = scene.add.tileSprite(x, y+10, 10, height-20, "platSide").setOrigin(0);
+        
+        let secondCorner = scene.add.sprite(x+width-10, y, "platCorner");
+        secondCorner.angle = 90;
+        secondCorner.setOrigin(0);
+        let secondSide = scene.add.tileSprite(x, y+10, 10, height-20, "platSide");
+        secondCorner.angle = 90;
+        secondSide.setOrigin(0);
+
         
         
 
